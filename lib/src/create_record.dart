ba17d476be0eba0fcf38600543c2a2f8a7df493c
Andrew Zuo <andrew@andrewzuo.com>
2024-05-25 14:01:51 -0400

    Forgot to add the actual create record file. Oops.

---
+++ lib/src/create_record.dart
@@ -0,0 +1,47 @@
+
+
+import 'dart:math';
+
+import 'package:pocketbase/pocketbase.dart';
+
+import 'get_records.dart';
+import 'pocketbase_offline_cache_base.dart';
+
+extension CreateWrapper on PbOfflineCache {
+  Future<void> createRecord(String collectionName, Map<String, dynamic> body, {
+    bool forceOffline = false,
+  }) async {
+
+    if (!dbAccessible || forceOffline) {
+
+      if (tableExists(db, collectionName)) {
+        String id = makePbId();
+        queueOperation("INSERT", collectionName, idToModify: id);
+        insertRecordsIntoLocalDb(db, collectionName, <RecordModel>[ RecordModel(
+          id: id,
+          created: DateTime.now().toString(),
+          updated: DateTime.now().toString(),
+          data: body,
+        ) ], logger);
+      }
+
+      return;
+    }
+
+    try {
+      RecordModel model = await (pb.collection(collectionName).create(body: body));
+      insertRecordsIntoLocalDb(db, collectionName, <RecordModel>[ model ], logger);
+    } on ClientException catch (_) {
+      return createRecord(collectionName, body, forceOffline: true);
+    }
+  }
+
+}
+
+final Random random = Random();
+
+String makePbId() {
+  const String chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+  return List.generate(15, (index) => chars[random.nextInt(chars.length)]).join();
+}
