09dca6f5e8dbd481ea99efe4507556a977e3004e
Andrew Zuo <andrew@andrewzuo.com>
2024-05-25 13:59:46 -0400

    Show these files as well

--- lib/pocketbase_offline_cache.dart
+++ lib/pocketbase_offline_cache.dart
@@ -4,3 +4,5 @@ export 'src/pocketbase_offline_cache_base.dart' show PbOfflineCache;
 export 'src/get_records.dart' show ListWrapper;
 export 'src/count_records.dart' show CountWrapper;
 export 'src/update_record.dart' show UpdateWrapper;
+export 'src/delete_record.dart' show DeleteWrapper;
+export 'src/create_record.dart' show CreateWrapper;
