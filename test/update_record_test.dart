64536fc6253eb5b5331d97e32a3d0acf936d90e0
Andrew Zuo <andrew@andrewzuo.com>
2024-05-26 23:11:46 -0400

    Actually added a few more test cases

--- test/update_record_test.dart
+++ test/update_record_test.dart
@@ -5,5 +5,7 @@ import 'package:test/test.dart';
 void main() {
 	test("makePbFilter", () {
 		expect(makePbFilter(("feed_url = ? && id_from_feed = ?", <Object>[ "https://www.reddit.com/r/rss/.rss?rdt=123", "t3_1cue8dv" ])), "feed_url = 'https://www.reddit.com/r/rss/.rss?rdt=123' && id_from_feed = 't3_1cue8dv'");
+		expect(makePbFilter(("id = 123", <Object>[ ])), "id = 123");
+		expect(makePbFilter(("id = ?", <Object>[ " 1 2 3 " ])), "id = ' 1 2 3 '");
 	});
 }
