6a15b8a98af7f1b479139e6647052cdff224f497
Andrew Zuo <andrew@andrewzuo.com>
2024-05-26 20:52:40 -0400

    Added GetOneWrapper

--- lib/pocketbase_offline_cache.dart
+++ lib/pocketbase_offline_cache.dart
@@ -3,5 +3,6 @@ export 'src/count_records.dart' show CountWrapper;
 export 'src/create_record.dart' show CreateWrapper;
 export 'src/delete_record.dart' show DeleteWrapper;
 export 'src/get_records.dart' show ListWrapper;
+export 'src/get_single_record.dart' show GetOneWrapper;
 export 'src/pocketbase_offline_cache_base.dart' show PbOfflineCache, QuerySource;
 export 'src/update_record.dart' show UpdateWrapper;
---
+++ lib/src/get_single_record.dart
@@ -0,0 +1,61 @@
+
+import 'dart:core';
+
+import 'package:logger/logger.dart';
+import 'package:pocketbase/pocketbase.dart';
+import 'package:sqlite3/sqlite3.dart';
+
+import 'get_records.dart';
+import 'pocketbase_offline_cache_base.dart';
+
+extension GetOneWrapper on PbOfflineCache {
+	Future<Map<String, dynamic>?> getSingleRecord(String collectionName, String id, {
+		QuerySource source = QuerySource.any,
+	}) async {
+		if (source != QuerySource.server && (!dbAccessible || source == QuerySource.client)) {
+			if (tableExists(db, collectionName)) {
+				final ResultSet results = selectBuilder(db, collectionName, maxItems: 1, filter: ("id = ?", <Object>[ id ]));
+				final List<Map<String, dynamic>> data = <Map<String, dynamic>>[];
+
+				for (final Row row in results) {
+					final Map<String, dynamic> entryToInsert = <String, dynamic>{};
+					for (final MapEntry<String, dynamic> data in row.entries) {
+						if (data.key.startsWith("_offline_bool_")) {
+							entryToInsert[data.key.substring(14)] = data.value == 1 ? true : false;
+						} else {
+							entryToInsert[data.key] = data.value;
+						}
+					}
+					data.add(entryToInsert);
+				}
+
+				return data.first;
+			}
+
+			return <String, dynamic>{};
+		}
+
+		try {
+			final RecordModel record = await pb.collection(collectionName).getOne(id);
+
+			insertRecordsIntoLocalDb(db, collectionName, <RecordModel>[ record ], logger);
+
+			final Map<String, dynamic> data = record.data;
+
+			data["id"] = record.id;
+			data["created"] = record.created;
+			data["updated"] = record.updated;
+
+			return data;
+		} on ClientException catch (e) {
+			if (!e.toString().contains("refused the network connection")) {
+				rethrow;
+			}
+			if (source == QuerySource.any) {
+				return getSingleRecord(collectionName, id, source: QuerySource.client);
+			} else {
+				return <String, dynamic>{};
+			}
+		}
+	}
+}
