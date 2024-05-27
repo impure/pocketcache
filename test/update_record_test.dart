7002e9b76d84f5c22cf8a36cc6ea3b91bed55a40
Andrew Zuo <andrew@andrewzuo.com>
2024-05-26 23:10:17 -0400

    Forgot to add this test

---
+++ test/update_record_test.dart
@@ -0,0 +1,9 @@
+
+import 'package:pocketbase_offline_cache/src/get_records.dart';
+import 'package:test/test.dart';
+
+void main() {
+	test("makePbFilter", () {
+		expect(makePbFilter(("feed_url = ? && id_from_feed = ?", <Object>[ "https://www.reddit.com/r/rss/.rss?rdt=123", "t3_1cue8dv" ])), "feed_url = 'https://www.reddit.com/r/rss/.rss?rdt=123' && id_from_feed = 't3_1cue8dv'");
+	});
+}
